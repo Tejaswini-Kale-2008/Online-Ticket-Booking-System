8015b89f1bf547b44b1a7c548cfbfa3283af9ded
Tejaswini-Kale-2008 <kaletejaswini36@gmail.com>
2025-07-10 19:38:03 +0530

    Update TicketDAO.java

--- src/main/corejavap/ticketbooking/TicketDAO.java
+++ src/main/corejavap/ticketbooking/TicketDAO.java
@@ -1,12 +1,12 @@
 package ticketbooking.dao;
 import java.sql.*;
-import java.ticketbooking.ticket;
-import ticketbooking.DBconnection;
+import java.ticketbooking.Ticket;
+import ticketbooking.DataBaseConnection;
 
 public class TicketDAO {
     public void saveTicket(Ticket ticket) {
         String sql = "INSERT INTO tickets (name, destination, price) VALUES (?, ?, ?)";
-        try (Connection conn = DBConnection.getConnection();
+        try (Connection conn = DataBaseConnection.getConnection();
              PreparedStatement stmt = conn.prepareStatement(sql)) {
             stmt.setString(1, ticket.getName());
             stmt.setString(2, ticket.getDestination());
